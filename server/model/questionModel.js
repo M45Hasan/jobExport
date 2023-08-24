766a56aa73c13ed8bfb63a18d2db6c000d98972b
M45Hasan <mmhasan045@gmail.com>
2023-08-24 17:30:38 +0600

    api 4

--- server/model/questionModel.js
+++ server/model/questionModel.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const app = express();
+
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-
-const userQuestion = new Schema({
+const newQuestion = new Schema({
+    examSerial:{type:String},
+    examId:{type:Schema.Types.ObjectId,ref:"Exam"},
+    whatIsTheQuestion:{type:String},
+    optionA:{type:String},
+    optionB:{type:String},
+    optionC:{type:String},
+    optionD:{type:String},
+    rightAnsOne:{type:String,require:true},
+    rightAnsTwo:{type:String,require:true},
+    ansDetail:{type:String},
+    rightMark:{type:Number},
+    wrongMark:{type:Number},
+   
 
 })
 
-module.exports=mongoose.model("Question",userQuestion)
+module.exports=mongoose.model("Question",newQuestion)
