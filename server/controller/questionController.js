ad89614436c13904606c41459d3c15435f5e6e69
M45Hasan <mmhasan045@gmail.com>
2023-08-27 16:21:32 +0600

    newApi

--- server/controller/questionController.js
+++ server/controller/questionController.js
@@ -19,33 +19,36 @@ const createQuestion = async (req, res) => {
     examSerial,
   } = req.body;
 
-  const saerch = await Exam.find({ examSerial });
-  res.send(saerch[0].examSerial);
   try {
-    // if (saerch.length != 0) {
-    //   const newQuestion = new Question({
-    //     examTrack: examSerial,
-    //     whatIsTheQuestion,
-    //     optionA,
-    //     optionB,
-    //     optionC,
-    //     optionD,
-    //     rightAnsOne,
-    //     rightAnsTwo,
-    //     ansDetail,
-    //     rightMark,
-    //     wrongMark,
-    //   });
-    //   //   newQuestion.save();
-    //   //   await Exam.findByIdAndUpdate(
-    //   //     { _id:saerch[0]._id },
-    //   //     { $push: { qestionList: newQuestion._id } },
-    //   //     { new: true }
-    //   //   );
-    //   //   res.status(201).json(newQuestion);
-    // } else {
-    //   res.status(401).json({ error: "Already" });
-    // }
+    const saerch = await Exam.find({ examSerial });
+    if (saerch.length != 0) {
+      const newQuestion = new Question({
+        examTrack: examSerial,
+        whatIsTheQuestion,
+        optionA,
+        optionB,
+        optionC,
+        optionD,
+        rightAnsOne,
+        rightAnsTwo,
+        ansDetail,
+        rightMark,
+        wrongMark,
+      });
+      newQuestion.save();
+      await Exam.findByIdAndUpdate(
+        { _id: saerch[0]._id },
+        { $push: { qestionList: newQuestion._id } },
+        { new: true }
+      );
+      await Question.findOneAndUpdate(
+        { _id: newQuestion._id },
+        { $push: { examId: saerch[0]._id } }
+      );
+      res.status(201).json(newQuestion);
+    } else {
+      res.status(401).json({ error: "Already" });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "An error occurred" });
