da39b96bb3cfc6f11dc64d908a2fe5ff06ab3c9b
weerotheme <weerotheme1@gmail.com>
2023-09-05 16:52:03 +0600

    daily quiz, dictonary

--- jobExpertFront/tailwind.config.js
+++ jobExpertFront/tailwind.config.js
@@ -2,11 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      primary: "#26A4DE",
-      secondary: "#EAE8E8",
+    extend: {
+      colors: {
+        primary: "#26A4DE",
+        secondary: "#EAE8E8",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
