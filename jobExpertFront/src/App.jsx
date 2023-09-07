dc7404c3b3af1e947ed0a20db868187b2cc63937
M45Hasan <mmhasan045@gmail.com>
2023-09-07 20:23:59 +0600

    push index

--- jobExpertFront/src/App.jsx
+++ jobExpertFront/src/App.jsx
@@ -20,6 +20,8 @@ import DailyQuiz from "./pages/DailyQuiz";
 import Dictionary from "./pages/Dictionary";
 import PremiumZone from "./pages/PremiumZone";
 import paymentReq from "./pages/paymentreq";
+import paymentSucc from "./pages/paymentSucc";
+import paymentFail from "./pages/paymentFail";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
@@ -29,6 +31,8 @@ const router = createBrowserRouter(
       <Route path="/verify" element={<Verify />}></Route>
      
       <Route path="/jobexpart" element={<Rootlayout />}>
+      <Route path="ssl-request" element={<paymentReq/>}></Route>
+      <Route path="ssl-payment-success" element={<paymentSucc/>}></Route>
       <Route path="ssl-request" element={<paymentReq/>}></Route>
         <Route index element={<Leanding />}></Route>
         <Route path="examZone" element={<ExamZone />}></Route>
