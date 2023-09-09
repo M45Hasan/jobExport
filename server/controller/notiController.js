edb9535b16936dfcbf0ec0269f22be1409cda52e
weerotheme <weerotheme1@gmail.com>
2023-09-09 17:16:01 +0600

    fix

--- server/controller/notiController.js
+++ server/controller/notiController.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const app = express();
 
-const Notification =require("../model/notiModel")
+const Notification = require("../model/notiModel");
 
+const getNoti = async (req, res) => {
+  try {
+    const noti = await Notification.find({});
+    console.log(noti);
 
-const getNoti = async( req,res)=>{
-    try{
-        const noti = await Notification.find()
-
-        if(noti.length !==0){
-            res.status(404).send(noti)
-        }
-    }catch(error){
-        res.status(500).json({error:error.cdoe})
+    if (noti.length !== 0) {
+      res.status(200).send(noti);
+    } else {
+      res.status(404).josn({ error: "no Notification" });
     }
-}
+  } catch (error) {
+    res.status(500).json({ error: error.cdoe });
+  }
+};
 
-module.exports=getNoti
+module.exports = getNoti;
