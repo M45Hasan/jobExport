8d8d7f63fb282f71fefe00da5bb0f7aa17cf4deb
M45Hasan <mmhasan045@gmail.com>
2023-09-09 17:13:53 +0600

    new0909

--- server/controller/notiController.js
+++ server/controller/notiController.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const app = express();
 
-const Notification =require("../model/notiModel")
+const Notification = require("../model/notiModel");
 
+const getNoti = async (req, res) => {
+  try {
+    const noti = await Notification.find();
 
-const getNoti = async( req,res)=>{
-    try{
-        const noti = await Notification.find()
-
-        if(noti.length !==0){
-            res.status(404).send(noti)
-        }
-    }catch(error){
-        res.status(500).json({error:error.cdoe})
+    if (noti.length !== 0) {
+      res.status(404).json({ error: "No Notification" });
+    } else {
+      res.status(200).send(noti);
     }
-}
+  } catch (error) {
+    res.status(500).json({ error: error.cdoe });
+  }
+};
 
-module.exports=getNoti
+module.exports = getNoti;
